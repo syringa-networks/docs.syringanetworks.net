dc0fb9386fd673ad9980c1449e1b6240d6adffae
Shawn Olson <shawn@fencepencil.com>
2020-06-10 16:31:03 -0600

    remove syslog

--- src/components/PageSections/Management.vue
+++ src/components/PageSections/Management.vue
@@ -56,8 +56,6 @@
   !
   logging source-interface Loopback0 vrf SYG
   logging host 10.10.10.82 vrf CUST
-  logging host 192.168.7.235 vrf SYG
-  logging host 192.168.71.224 vrf SYG
   logging host 192.168.80.16 vrf SYG
   !
   line con 0
